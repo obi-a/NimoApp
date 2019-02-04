92b860e3de86d8d862d7a16a44116a2af61244a3
RainMaker <obioraakubue@yahoo.com>
2019-02-03 20:57:38 -0500

    update

--- App.tsx
+++ App.tsx
@@ -9,8 +9,75 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Status } from "./components/nimo";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicatorIOS,
+  Button,
+  TextInput,
+  Alert,
+  ScrollView,
+  Image,
+  FlatList,
+  SectionList,
+  NavigatorIOS
+} from 'react-native'
+import { Status } from "./src/lib/nimo";
+import { RkTheme, RkTabSet, RkTab } from 'react-native-ui-kitten';
+import { Ionicons } from 'react-native-vector-icons'; // 6.2.2
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  container: {
+   flex: 1,
+   paddingTop: 22,
+   backgroundColor: 'white'
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  buttonContainer: {
+    margin: 20,
+    color: 'blue'
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  }
+});
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,34 +87,137 @@ const instructions = Platform.select({
 });
 
 interface Props {}
-export default class App extends Component<Props> {
+interface State {text: string}
+
+
+export default class NavigatorIOSApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.instructions}>{Status.Todo}</Text>
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: 'My Initial Scene',
+          passProps: {index: 1},
+        }}
+        style={{flex: 1}}
+      />
+    );
+  }
+}
+
+class TodoScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+        <Text>Todo!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+class InProgressScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+        <Text>In Progress!</Text>
+      </View>
+    );
+  }
+}
+
+class DoneScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+        <Text>Done!</Text>
+      </View>
+    );
+  }
+}
+
+interface Props {name: string, badgeCount: number, color: string; size: number }
+export class IconWithBadge extends React.Component<Props> {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // /If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const HomeIconWithBadge = props => {
+  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'To do') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+    // We want to add badges to home tab icon
+    IconComponent = HomeIconWithBadge;
+  } else if (routeName === 'In Progress') {
+    iconName = `ios-options${focused ? '' : '-outline'}`;
+  } else if (routeName === 'Done') {
+    iconName = `ios-options${focused ? '' : '-outline'}`;
+  }
+
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+const TabNavigator = createBottomTabNavigator({
+  'To do': { screen: TodoScreen },
+  'In Progress': { screen: InProgressScreen },
+  'Done': { screen: DoneScreen }
 });
+
+
+
+export default createAppContainer(TabNavigator);
+/*
+export default class App extends Component<Props, State> {
+
+  constructor(props :any) {
+    super(props);
+    //const state :CommonState = {text: ''};
+    this.state = {text: ''};
+  }
+
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+*/
